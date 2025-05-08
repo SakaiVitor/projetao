a4be4d1fc4177c03e666765c7ce01f00de0caaa1
Evelyn Henriques <evelynhenriques@ime.eb.br>
2025-05-08 18:42:19 -0300

    implementação janela de prompt

--- player/controller.py
+++ player/controller.py
@@ -53,13 +53,18 @@ class PlayerController:
         for key in self.keys:
             self.app.accept(key, self.set_key, [key, True])
             self.app.accept(f"{key}-up", self.set_key, [key, False])
-        self.app.accept("enter", self.toggle_input)
+        self.app.accept("tab", self.toggle_prompt)
 
     def set_key(self, key, value):
         self.keys[key] = value
 
-    def toggle_input(self):
-        self.moving = not self.moving
+    def toggle_prompt(self):
+        if self.app.hud.entry:
+            self.app.hud.close_prompt()
+            self.moving = True
+        else:
+            self.app.hud.show_prompt()
+            self.moving = False
 
     def lock_mouse(self):
         props = WindowProperties()
